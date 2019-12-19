9a958c46606085770e4013483e5b51f1714e9e4a
Alin Mircea Crachina <crachina_alin@yahoo.com>
2019-12-19 15:19:10 +0200

    Add files via upload

---
+++ Programming Principles and Practice Using C++/Chapter 3/drill_chapter3_pct1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<stdlib.h>
+#include<conio.h>
+
+using namespace std;
+string first_name;
+
+int main()
+{
+    system("cls");
+    cout<<"Please enter your name: ";
+    cin>>first_name;
+    cout<<"Hello, "<<first_name;
+
+    getch();
+}
