29bbaf253ed481ee7c5353351ce0ad72786a8e11
Alin Mircea Crachina <crachina_alin@yahoo.com>
2019-12-19 16:14:15 +0200

    Update drill_chapter3_pct2.cpp

--- Programming Principles and Practice Using C++/Chapter 3/drill_chapter3_pct2.cpp
+++ Programming Principles and Practice Using C++/Chapter 3/drill_chapter3_pct2.cpp
@@ -1,3 +1,10 @@
+//write a program that produces a simple form letter based on user input. 
+//begin by typing the code from prompting a user to enter
+//his or her first name and writing “Hello, first_name” where first_name is
+//the name entered by the user.
+//Then modify your code as follows: change the prompt to “Enter the name of the person you want to write to” and
+//change the output to “Dear first_name,”.
+
 #include<iostream>
 #include<stdlib.h>
 #include<conio.h>
